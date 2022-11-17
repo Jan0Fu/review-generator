838618545042211f4729144a26a6184e9125b0c8
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-17 17:12:25 +0100

    Added basic functionality

---
+++ script.js
@@ -0,0 +1,18 @@
+const reviews = [{name: "Jane Doe",
+review: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos aliquid in tempore cupiditate, ipsam totam sit nihil. Unde, voluptatum officiis!"
+},
+{name: "Joe Black",
+review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minima commodi asperiores? Consequatur quibusdam doloremque nam?",
+},
+{name: "Ann Smith",
+review: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod eos aliquid in tempore cupiditate, ipsam totam sit nihil. Unde, voluptatum officiis!"
+},
+{name: "John Gray",
+review: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minima commodi asperiores? Consequatur quibusdam doloremque nam?",
+},]
+
+function ranNum() {
+    return Math.floor(Math.random() * reviews.length);
+}
+
+
